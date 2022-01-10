c2e0bcd33fba7f8ee1567126fe707216a91b1cce
Polina Ganina <poliganina@gmail.com>
2022-01-10 20:26:59 +0300

    catalog in process

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+// let answer = confirm ('Вам есть 18 лет?');
+// if (answer) {
+//     console.log ('Come in')
+// } else {
+//     console.log ('Get out')
+// }
+
+for (let i = 1; i < 8; i++) {
+    console.log (i);
+}
+
+function () {
+    console.log (222)
+}
